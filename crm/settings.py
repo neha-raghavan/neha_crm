3b1bcf2333e9fc39eb90b21781d36c402fe4f752
neha-raghavan <rpneha11@gmail.com>
2021-08-28 13:56:18 +0530

    last commit whitenoise static

--- crm/settings.py
+++ crm/settings.py
@@ -137,9 +137,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles/')
 
 STATIC_URL = '/static/'
+
+
+
 
 MEDIA_URL='/images/'
 
@@ -147,6 +149,7 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
 
